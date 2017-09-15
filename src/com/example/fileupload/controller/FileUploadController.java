a9a9cd2ffb83819cbfc2c599fac0457ad9aa6a01
bit-user <bit-user@DESKTOP-C1QR4BV>
2017-09-15 15:40:39 +0900

    완료

--- src/com/example/fileupload/controller/FileUploadController.java
+++ src/com/example/fileupload/controller/FileUploadController.java
@@ -1,7 +1,33 @@
 package com.example.fileupload.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.fileupload.service.FileUploadService;
 
 @Controller
 public class FileUploadController {
+	@Autowired
+	private FileUploadService fileUploadService;
+	
+	@RequestMapping( "/form" )
+	public String form() {
+		return "form";
+	}
+	
+	@RequestMapping( "/upload" )
+	public String upload(
+		@RequestParam( "email" ) String email,
+		@RequestParam( "file1" ) MultipartFile file1,
+		Model model) {
+		
+		String url = fileUploadService.restore(file1);
+		model.addAttribute( "url", url );
+		
+		return "result";
+	}
 }
